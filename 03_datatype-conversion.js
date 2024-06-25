a9d1064efb0a8373d26b96d591031ec939127ff2
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-06-25 13:50:19 +0530

    conversion of datatypes

---
+++ 03_datatype-conversion.js
@@ -0,0 +1,17 @@
+let score = "90" ;
+let cal = Number(score)
+console.log(`${typeof(score)}`);
+
+console.log(`${typeof cal}`);
+
+let a = "100bc" ;
+let c = Number(a) ;
+console.log(`${typeof a}`);
+console.log(`${typeof c}`);
+console.log(`because it can not be converted into number ${c}`);
+
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1
+// false => 0
