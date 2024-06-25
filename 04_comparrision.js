ffb1e4d913cf98096307c089bab4913019bdd787
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-06-25 14:39:22 +0530

    comparrisoin in js

---
+++ 04_comparrision.js
@@ -0,0 +1,13 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log( null >= 0);
+
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log( undefined < 0);
+console.log( undefined >= 0);
+
+
+
