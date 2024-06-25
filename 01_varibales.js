c43984d2761274f69e86acc001cf27122903ec42
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-06-25 13:40:57 +0530

    data types in js

--- 01_varibales.js
+++ 01_varibales.js
@@ -2,3 +2,5 @@ const name = "Divyansh Agarwal" ;
 var ph = 9027395702 ;
 city = "delhi" ;
 console.table([name, ph, city]) ;
+let a ;
+console.log(`${a}`);
---
+++ 02_datatypes.js
@@ -0,0 +1,10 @@
+let name = "Divyansh" ;
+let age = 21 ;
+let isLoggedIn = false ;
+
+
+// number, bigint, string, boolean, numm, undefined, symbol
+
+//type of null is an object while undefined is undefined 
+
+// objects, 
