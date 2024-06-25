9164412f65da0b536790e79b1f502c7397239e8a
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-06-25 16:55:42 +0530

    data type summary

--- 03_datatype-conversion&operations.js
+++ 03_datatype-conversion&operations.js
@@ -22,3 +22,24 @@ console.log(1 + 2 + "2");
 
 
 
+// types of data types
+// primitive and non-primitive 
+// changes takes place in copy of the varible
+// primitive - 7 
+// 1- string 
+// 2 - number 
+// 3 - boolean 
+// 4 - null
+// 5 - undefined 
+// 6 - Symbol 
+// 7 - bigint 
+
+// refrence (non-primitive)
+// arrays
+// objects 
+// functions
+
+let id1 = Symbol("123") ;
+let id2 = Symbol("123") ;    // symbol always return a unique symbol or value    
+
+console.log(id1 == id2);
