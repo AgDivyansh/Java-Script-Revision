6dedaf2381b351da5ec86a87feec10b92cd2542f
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-06-26 11:06:06 +0530

    numbers in js

---
+++ numberes.js
@@ -0,0 +1,11 @@
+const n = 23.8966 ;
+// console.log(n.toPrecision(3));
+
+
+const min = 10 ;
+const max = 20 ;
+
+// console.log((Math.random() * (max-min+1)) + min);/
+console.log(Math.random() * (max-min+1) + min);
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
