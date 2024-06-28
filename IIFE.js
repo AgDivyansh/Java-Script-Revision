497552cc3cff4007149e9dc8aded30ca995e3f91
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-06-28 15:53:21 +0530

    immediatly invoke function (IIEF)

---
+++ IIFE.js
@@ -0,0 +1,15 @@
+function gym ()
+{
+    console.log(`Deadlift`);
+    
+}
+gym() ;
+
+// iife  immediately invoked function
+(
+    function dead ()
+    {
+        console.log(`iife deadlift`);
+        
+    }
+) () ;
