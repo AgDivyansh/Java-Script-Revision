38cca3d3de4d04a9e6125caf6805c9b2b222d35b
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-07-02 14:50:04 +0530

    loops in js

---
+++ loops/01_loops.js
@@ -0,0 +1,18 @@
+// for 
+for (let i = 0; i < 10; i++) {
+    // const element = [i];
+    // console.log(i);
+    
+    
+}
+
+
+
+const arr = ["js", "ruby", "java", "c++"] ;
+                // Element, index, full array 
+                // this is specified
+arr.forEach((item, ind, arr) => {
+  console.log(item, ind, arr);
+  
+}
+)
