96300a283b1628a5814a7299dba3683f23097298
Divyansh Agarwal <v-divagarwal@microsoft.com>
2024-07-03 13:34:56 +0530

    loops in js

--- loops/01_loops.js
+++ loops/01_loops.js
@@ -16,3 +16,14 @@ arr.forEach((item, ind, arr) => {
   
 }
 )
+
+const nums = [] ;
+for (let i = 0; i < 10 ; i++) {
+    // const element = 10[i
+    nums.push(i) ;
+}
+console.log(nums);
+
+console.log(nums.filter((value) => 
+  value > 5
+));
